bd23bddde87722cfbc3fbe669952b9d2b229f968
tuplock <tuplock@gmail.com>
2016-11-17 23:30:44 +0800

    add

---
+++ src/main/java/com/jd/CloverApplication.java
@@ -0,0 +1,12 @@
+package com.jd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloverApplication.class, args);
+	}
+}
